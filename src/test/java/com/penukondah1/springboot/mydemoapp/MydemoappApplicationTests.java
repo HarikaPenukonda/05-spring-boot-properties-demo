d3e30d7fd8dbac39785846510d2883da49b9464c
harika penukonda <penukondaharika@gmail.com>
2020-10-05 11:31:18 -0500

    initial commit

---
+++ src/test/java/com/penukondah1/springboot/mydemoapp/MydemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penukondah1.springboot.mydemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
