cc77cef4479d0383165256af8a749b20dc3eaa92
harika penukonda <penukondaharika@gmail.com>
2020-10-05 11:43:58 -0500

    Adding the properties values

--- src/main/java/com/penukondah1/springboot/mydemoapp/rest/FunRestController.java
+++ src/main/java/com/penukondah1/springboot/mydemoapp/rest/FunRestController.java
@@ -2,11 +2,25 @@ package com.penukondah1.springboot.mydemoapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// inject properties for: coach.name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	// expose new end point for "team info"
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return " Coach: " + coachName + " Team name: " + teamName ;
+	}
 	
 	// expose "/" that return "Hello World"
 	@GetMapping("/")
@@ -14,7 +28,7 @@ public class FunRestController {
 		return "Hello World! Time on server is " + LocalDateTime.now();
 	}
 	
-	// expose a new end point for "workout"
+	// expose a new end point for "work out"
 	@GetMapping("/workout")
 	public String getDailyWorkout() {
 		return "Run a hard 5K!";
